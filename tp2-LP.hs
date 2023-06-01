6e2f3b83e0d20acb7398b8e8d6a0678cbfd079f2
Gabriel Duque Schiffner Oliveira <gabrielduqueschiffner@gmail.com>
2023-06-01 14:55:34 -0300

    Adicionada a randomizacao das posicoes das bombas, alem da criacao de tratamento para inputs erroneos

--- tp2-LP.hs
+++ tp2-LP.hs
@@ -1,7 +1,7 @@
 -- Nome Matheus
 -- Nome Gabriel Duque Schiffner Oliveira - 201965033a
 
---import System.Random
+import System.Random
 import System.IO
 
 
@@ -26,20 +26,20 @@ criaTabuleiro quantLinhas quantColunas = replicate quantLinhas (replicate quantC
 
 -- Função para gerar posições aleatórias para as minas
 
---geraPosicaoMinas :: Int -> Int -> Int -> StdGen -> [Posicao]
---geraPosicaoMinas quantLinhas quantColunas quantidadeMinas gen =
- -- take quantidadeMinas $ randomPositions gen
---  where
- --   indices = [(x, y) | x <- [0..quantLinhas-1], y <- [0..quantColunas-1]]
-  --  randomPositions = shuffle indices
+geraPosicaoMinas :: Int -> Int -> Int -> StdGen -> [Posicao]
+geraPosicaoMinas quantLinhas quantColunas quantidadeMinas gen =
+  take quantidadeMinas $ randomPositions gen
+  where
+    indices = [(x, y) | x <- [0..quantLinhas-1], y <- [0..quantColunas-1]]
+    randomPositions = shuffle indices
 
 -- Função para embaralhar uma lista usando um gerador de números aleatórios
- --shuffle :: RandomGen g => [a] -> g -> [a]
---shuffle [] _ = []
---shuffle lst gen =
- -- let (index, newGen) = randomR (0, length lst - 1) gen
- --     (rest, (selected:remaining)) = splitAt index lst
- -- in selected : shuffle (rest ++ remaining) newGen
+shuffle :: RandomGen g => [a] -> g -> [a]
+shuffle [] _ = []
+shuffle lst gen =
+  let (index, newGen) = randomR (0, length lst - 1) gen
+      (rest, (selected:remaining)) = splitAt index lst
+  in selected : shuffle (rest ++ remaining) newGen
 
    
 
@@ -62,41 +62,76 @@ printCampoMinado campoMinadoGerado = mapM_ (putStrLn . unwords . map showRow) ca
     showRow True = "X"
     showRow False = "_"   
 
+
+-- Função para verificar se uma posição é válida no campo minado
+--posicaoValida :: CampoMinado -> Posicao -> Bool
+--posicaoValida campoMinado (x, y) =
+  --x >= 0 && x < length campoMinado && y >= 0 && y < length (head campoMinado)
+
+-- Função para contar o número de minas ao redor de uma posição
+--contarMinasAoRedor :: CampoMinado -> Posicao -> Int
+--contarMinasAoRedor campoMinado (x, y) =
+--  length $ filter (== True) $ map (campoMinado !!) posicoesVizinhas
+  --where
+    --posicoesVizinhas =
+      --[ (x - 1, y),
+        --(x, y - 1),
+        --(x, y + 1),
+        --(x + 1, y)
+      --]
+    --posicoesValidas = filter (posicaoValida campoMinado) posicoesVizinhas
+
+-- Funcao das jogadas
+
+--jogadaCampoMinado :: CampoMinado -> [Posicao] -> Char -> CampoMinado
+--jogadaCampoMinado campoMinadoGerado posicaoJogada tipoJogada =
+ --   | tipoJogada == +_
+ --   | tipoJogada == -_
+ --   | Otherwise = 
+
 -- Funcao principal para selecionar o tamanho do tabuleiro, quantidade de minas, receber os dados e comandos de menu
 
 main :: IO ()
 main = do
-    -- Quantidade de Linhas do tabuleiro
-    putStrLn "Informe a quantidade de linhas do tabuleiro"
-    quantLin <- getLine
-    let quantLinhas = read quantLin :: Int
-
+  -- Quantidade de linhas do tabuleiro
+  putStrLn "Informe a quantidade de linhas do tabuleiro"
+  quantLin <- getLine
+  let quantLinhas = read quantLin :: Int
+  
+  -- Verifica se a quantidade de linhas é válida
+  let linhasValidas = quantLinhas > 0
+  if not linhasValidas then putStrLn "Quantidade de linhas inválida!"
+  else do
     -- Quantidade de colunas do tabuleiro
     putStrLn "Informe a quantidade de colunas do tabuleiro"
     quantCol <- getLine
     let quantColunas = read quantCol :: Int
 
-    -- Quantidade de minas
-    putStrLn ("Informe a quantia de minas entre 1 e " ++ show ((quantLinhas * quantColunas) `div` 2))
-    quantidMinas <- getLine
-    let quantidadeMinas = read quantidMinas :: Int
+    -- Verifica se a quantidade de colunas é válida
+    let colunasValidas = quantColunas > 0
+    if not colunasValidas then putStrLn "Quantidade de colunas inválida!"
+    else do
+      -- Quantidade de minas
+      putStrLn ("Informe a quantidade de minas entre 1 e " ++ show ((quantLinhas * quantColunas) `div` 2))
+      quantMinas <- getLine
+      let quantidadeMinas = read quantMinas :: Int
 
-    -- Verifica se a quantidade de minas é válida
-    let minasValidas = quantidadeMinas > 0 && quantidadeMinas < (quantLinhas * quantColunas) `div` 2
-    if not minasValidas then putStrLn "Quantidade de minas inválida!" else do
+      -- Verifica se a quantidade de minas é válida
+      let minasValidas = quantidadeMinas > 0 && quantidadeMinas < (quantLinhas * quantColunas) `div` 2
+      if not minasValidas then putStrLn "Quantidade de minas inválida!"
+      else do
         -- Gera posições aleatórias para as minas
-        -- gen <- newStdGen
-        -- let posicaoMinas = geraPosicaoMinas quantLinhas quantColunas quantidadeMinas gen
-        let posicaoMinas = [(0, 0), (1, 1), (2, 2)] -- PARA TESTES, USAR PELO MENOS UM TABULEIRO 5x5
+        gen <- newStdGen
+        let posicaoMinas = geraPosicaoMinas quantLinhas quantColunas quantidadeMinas gen
 
-        -- Função que gera o tabuleiro do campo minado
-        let campoMinadoGerado = criaTabuleiro quantLinhas quantColunas
+        -- Cria o campo minado vazio
+        let campoMinado = criaTabuleiro quantLinhas quantColunas
 
-        -- Função que gera as minas em posições aleatórias utilizando a quantidade passada pelo usuário
-        let atualizaTabuleiro = insereMinas campoMinadoGerado posicaoMinas
+        -- Insere as minas no campo minado
+        let campoMinadoComMinas = insereMinas campoMinado posicaoMinas
 
-        -- Printa e atualiza o campo Minado
-        printCampoMinado atualizaTabuleiro
+        -- Imprime o campo minado
+        printCampoMinado campoMinadoComMinas
 
 
 
